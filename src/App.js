6156b6b34a7b0124106614405ba6121419e98d74
YuSunjo <tnswh2023@gmail.com>
2020-08-19 20:12:25 +0900

    material ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React , {Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import {withStyles} from '@material-ui/core/styles'
+
+const styles = (theme) => ( 
+  {
+    root: {
+      width: '100%',
+      marginTop : theme.spacing.unit* 3,
+      overflowX: "auto"
+    },
+    table: {
+      minWidth: 1080,
+    }
+  }
+)
 
 class App extends Component {
   state={
     customers : [{
       id: 1,
-      image : 'https://placeimg.com/64/64/1',
+      image : 'https://placeimg.com/64/64/any',
       name: '홍길동',
       age : 25,
       gender :'남자',
@@ -14,36 +34,50 @@ class App extends Component {
     },
     {
       id: 2,
-      image : 'https://placeimg.com/64/64/2',
+      image : 'https://placeimg.com/64/64/any',
       name: 'sunjo',
       age : 25,
       gender :'남자',
-      job : '프로그래머',
+      job : '가영이남편',
     },
     {
       id: 3,
-      image : 'https://placeimg.com/64/64/3',
-      name: 'ga',
+      image : 'https://placeimg.com/64/64/any',
+      name: 'gayoung',
       age : 25,
       gender :'여자',
-      job : '가수',
+      job : '교수',
     },
   ]
   }
   
 
   render() {
+    const {classes} = this.props;
     return (
-      <div>
-        {this.state.customers.map(  (customers) => {
-          return (
-            <Customer key={customers.id} id={customers.id} image={customers.image} name={customers.name} age={customers.age} gender = {customers.gender} 
-            job = {customers.job} />
-          )
-        })}
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead> 
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>나이</TableCell> 
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+              {this.state.customers.map(  (customers) => {
+              return (
+                <Customer key={customers.id} id={customers.id} image={customers.image} name={customers.name} age={customers.age} gender = {customers.gender} 
+                job = {customers.job} />
+              )
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
     )
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,41 +1,47 @@
 import React ,{Component} from 'react';
-
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
 
 class Customer extends Component {
 
 
     render() {
         return (
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo age={this.props.age}  job={this.props.job} />
-            </div>
+            
+                <TableRow>
+                    <TableCell>{this.props.id}</TableCell>
+                    <TableCell><img src={this.props.image} alt="profile"></img></TableCell>
+                    <TableCell>{this.props.name}</TableCell>
+                    <TableCell>{this.props.age}</TableCell>
+                    <TableCell>{this.props.job}</TableCell>
+                </TableRow>
+                
         )
     }
 }
 
 
-class CustomerProfile extends Component {
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile"></img>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
+// class CustomerProfile extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <img src={this.props.image} alt="profile"></img>
+//                 <h2>{this.props.name}({this.props.id})</h2>
+//             </div>
+//         )
+//     }
+// }
 
-class CustomerInfo extends Component {
-    render() {
-        return (
-            <div>
-                {this.props.age}
-                {this.props.job}
-            </div>
-        )
-    }
-}
+// class CustomerInfo extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 {this.props.age}
+//                 {this.props.job}
+//             </div>
+//         )
+//     }
+// }
 
 
 export default Customer;
